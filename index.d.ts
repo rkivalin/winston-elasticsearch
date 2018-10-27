6aaca0bdede1287cacadd4a12262b7af13f8c809
Roman Kivalin <roman@kivalin.me>
2018-10-27 14:21:29 +0400

    fix definitions

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,27 @@
-declare module 'winston-elasticsearch' {
-  import TransportStream from 'winston-transport';
-  import elasticsearch from 'elasticsearch';
+import * as TransportStream from 'winston-transport';
+import * as elasticsearch from 'elasticsearch';
 
-  export interface LogData {
+declare class Elasticsearch extends TransportStream {
+  constructor(opts?: Elasticsearch.ElasticsearchTransportOptions);
+
+  query<T>(options: any, callback?: () => void): Promise<elasticsearch.SearchResponse<T>>;
+  query<T>(q: string): Promise<elasticsearch.SearchResponse<T>>;
+  getIndexName(opts: Elasticsearch.ElasticsearchTransportOptions): string;
+}
+
+declare namespace Elasticsearch {
+  interface LogData {
     message: any;
     level: string;
     meta: { [key: string]: any };
     timestamp?: string;
   }
 
-  export interface Transformer {
+  interface Transformer {
     (logData: LogData): any;
   }
 
-  export interface ElasticsearchTransportOptions extends TransportStream.TransportStreamOptions {
+  interface ElasticsearchTransportOptions extends TransportStream.TransportStreamOptions {
     timestamp?: () => string;
     level?: string;
     index?: string;
@@ -30,12 +38,6 @@ declare module 'winston-elasticsearch' {
     client?: elasticsearch.Client;
     clientOpts?: elasticsearch.ConfigOptions;
   }
-
-  export default class Elasticsearch extends TransportStream {
-    constructor(opts?: ElasticsearchTransportOptions);
-
-    query<T>(options: any, callback?: () => void): Promise<elasticsearch.SearchResponse<T>>;
-    query<T>(q: string): Promise<elasticsearch.SearchResponse<T>>;
-    getIndexName(opts: ElasticsearchTransportOptions): string;
-  }
 }
+
+export = Elasticsearch;
